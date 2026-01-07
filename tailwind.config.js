89f1b8b533f78b0aa4cdb3a25e2cca75386f25f8
Mohammed Mustafa Khan <53117363+MustafaKhan026@users.noreply.github.com>
2026-01-07 14:04:36 +0530

    tailwind config issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
-extend: {
-  fontFamily: {
-    poppins: "var(--font-poppins)",
-    inter: "var(--font-inter)",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "var(--font-poppins)",
+        inter: "var(--font-inter)",
+      },
+    },
   },
-}
+  plugins: [],
+};
